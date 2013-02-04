2ad4ecf0c3a6d4911a42e5774e10e4c0e33bef67
Matt Hootman <matt.hootman@cerner.com>
2013-02-04 11:16:24 -0600

    adding some doc

--- Gemfile
+++ Gemfile
@@ -10,7 +10,7 @@ gem 'jquery-rails', '2.0.2'
 
 group :development, :test do
   gem 'sqlite3', '1.3.6'
-  gem 'rspec-rails', '2.11.0'
+  gem 'rspec-rails', '2.12.1'
 end
 
 group :development do
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,83 +14,110 @@ require 'spec_helper'
 describe User do
 
   before do
-    @user = User.new(name: "Example User", email: "user@example.com", 
-                     password: "foobar", password_confirmation: "foobar")
+    @user = User.new(name: 'Example User', email: 'user@example.com',
+                     password: 'foobar', password_confirmation: 'foobar')
   end
 
   subject { @user }
 
-  it { should respond_to(:name) }
-  it { should respond_to(:email) }
-  it { should respond_to(:password_digest) }
-  it { should respond_to(:password) }
-  it { should respond_to(:password_confirmation) }
-  it { should respond_to(:remember_token) }
-  it { should respond_to(:admin) }
-  it { should respond_to(:authenticate) }
-  it { should respond_to(:microposts) }
-  it { should respond_to{:feed} }
-  it { should respond_to(:relationships) }
-  it { should respond_to(:followed_users) }
-
-  it { should be_valid }
-  it { should_not be_admin }
-
   # =============================
   # admin validation
   # =============================
-  describe "with admin attribute set to 'true'" do
-    before do
+  describe '#admin?' do
+    it 'is true if user is admin' do
       @user.save!
       @user.toggle!(:admin)
+      expect(@user).to be_admin
     end
-
-    it { should be_admin }
   end
 
-  # =============================
-  # token validation
-  # =============================
-  describe "remember token" do
-    before { @user.save }
-    its(:remember_token) { should_not be_blank }
-  end
-  
   # =============================
   # name validation
   # =============================
-  describe "when name is not present" do
-    before { @user.name = " " }
-    it { should_not be_valid }
-  end
+  describe '#name' do
+    it 'should invalidate user if empty' do
+      @user.name = " "
+      expect(@user).to_not be_valid
+    end
 
-  describe "when name is too long" do
-    before { @user.name = "a" * 51 }
-    it { should_not be_valid }
+    it 'should invalidate user if too long' do
+      @user.name = 'a' * 51
+      expect(@user).to_not be_valid
+    end
   end
 
   # =============================
   # password validation
   # =============================
-  describe "when password is not present" do
-	  before { @user.password = @user.password_confirmation = " " }
-	  it { should_not be_valid }
-	end
+  describe '#password' do
+    it "invalidates the user when missing" do
+      @user.password = @user.password.confirmation = " "
+      expect(@user).to_not be_valid
+    end
 
-	describe "when password doesn't match confirmation" do
-	  before { @user.password_confirmation = "mismatch" }
-	  it { should_not be_valid }
-	end
+    it 'invalidates the user when confirmation does not match' do
+      @user.password_confirmation = 'mismatch'
+      expect(@user).to_not be_valid
+    end
 
-	describe "when password confirmation is nil" do
-	  before { @user.password_confirmation = nil }
-	  it { should_not be_valid }
-	end
+    it 'invalidates the user when nil' do
 
-	describe "with a password that's too short" do
-	  before { @user.password = @user.password_confirmation = "a" * 5 }
-	  it { should be_invalid }
-	end
+    end
+
+    it 'does' do
+
+    end
+  end
+
+  describe "when password confirmation is nil" do
+    before { @user.password_confirmation = nil }
+    it { should_not be_valid }
+  end
+
+  describe "with a password that's too short" do
+    before { @user.password = @user.password_confirmation = "a" * 5 }
+    it { should be_invalid }
+  end
+
+  # it 'responds to the appropriate attributes' do
+  #   expect(@user).to respond_to(:name)
+  # end
+end
+  #it { should respond_to(:name) }
+#   it { should respond_to(:email) }
+#   it { should respond_to(:password_digest) }
+#   it { should respond_to(:password) }
+#   it { should respond_to(:password_confirmation) }
+#   it { should respond_to(:remember_token) }
+#   it { should respond_to(:admin) }
+#   it { should respond_to(:authenticate) }
+#   it { should respond_to(:microposts) }
+#   it { should respond_to{:feed} }
+#   it { should respond_to(:relationships) }
+#   it { should respond_to(:followed_users) }
+
+#   it { should be_valid }
+#   it { should_not be_admin }
+
+#   # =============================
+#   # admin validation
+#   # =============================
+#   describe "with admin attribute set to 'true'" do
+#     before do
+#       @user.save!
+#       @user.toggle!(:admin)
+#     end
+
+#     it { should be_admin }
+#   end
+
+#   # =============================
+#   # token validation
+#   # =============================
+#   describe "remember token" do
+#     before { @user.save }
+#     its(:remember_token) { should_not be_blank }
+#   end
 
 	describe "return value of authenticate method" do
 	  before { @user.save }
@@ -107,126 +134,126 @@ describe User do
 	    specify { user_for_invalid_password.should be_false }
 	  end
 	end
-  
-  # =============================
-  # email validation
-  # =============================
-  describe "when email is not present" do
-    before { @user.email = " " }
-    it { should_not be_valid }
-  end
 
-  describe "when email format is invalid" do
-    it "should be invalid" do
-      addresses = %w[user@foo,com user_at_foo.org example.user@foo.
-                     foo@bar_baz.com foo@bar+baz.com first.last!@you.gov]
-      addresses.each do |invalid_address|
-        @user.email = invalid_address
-        @user.should_not be_valid
-      end      
-    end
-  end
+#   # =============================
+#   # email validation
+#   # =============================
+#   describe "when email is not present" do
+#     before { @user.email = " " }
+#     it { should_not be_valid }
+#   end
 
-  describe "when email format is valid" do
-    it "should be valid" do
-      addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
-      addresses.each do |valid_address|
-        @user.email = valid_address
-        @user.should be_valid
-      end      
-    end
-  end
+#   describe "when email format is invalid" do
+#     it "should be invalid" do
+#       addresses = %w[user@foo,com user_at_foo.org example.user@foo.
+#                      foo@bar_baz.com foo@bar+baz.com first.last!@you.gov]
+#       addresses.each do |invalid_address|
+#         @user.email = invalid_address
+#         @user.should_not be_valid
+#       end
+#     end
+#   end
 
-  describe "when email address is already taken" do
-    before do
-      user_with_same_email = @user.dup
-      user_with_same_email.save
-    end
+#   describe "when email format is valid" do
+#     it "should be valid" do
+#       addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
+#       addresses.each do |valid_address|
+#         @user.email = valid_address
+#         @user.should be_valid
+#       end
+#     end
+#   end
 
-    it { should_not be_valid }
-  end
+#   describe "when email address is already taken" do
+#     before do
+#       user_with_same_email = @user.dup
+#       user_with_same_email.save
+#     end
 
-  describe "when email address is already taken" do
-    before do
-      user_with_same_email = @user.dup
-      user_with_same_email.email = @user.email.upcase
-      user_with_same_email.save
-    end
+#     it { should_not be_valid }
+#   end
 
-    it { should_not be_valid }
-  end
+#   describe "when email address is already taken" do
+#     before do
+#       user_with_same_email = @user.dup
+#       user_with_same_email.email = @user.email.upcase
+#       user_with_same_email.save
+#     end
 
-  # =============================
-  # micropost validation
-  # =============================
-  describe "micropost associations" do
+#     it { should_not be_valid }
+#   end
 
-    before { @user.save }
-    let!(:older_micropost) do 
-      FactoryGirl.create(:micropost, user: @user, created_at: 1.day.ago)
-    end
-    let!(:newer_micropost) do
-      FactoryGirl.create(:micropost, user: @user, created_at: 1.hour.ago)
-    end
+#   # =============================
+#   # micropost validation
+#   # =============================
+#   describe "micropost associations" do
 
-    it "should have the right microposts in the right order" do
-      @user.microposts.should == [newer_micropost, older_micropost]
-    end
+#     before { @user.save }
+#     let!(:older_micropost) do
+#       FactoryGirl.create(:micropost, user: @user, created_at: 1.day.ago)
+#     end
+#     let!(:newer_micropost) do
+#       FactoryGirl.create(:micropost, user: @user, created_at: 1.hour.ago)
+#     end
 
-    it "should destroy associated microposts" do
-      microposts = @user.microposts.dup
-      @user.destroy
-      microposts.should_not be_empty
-      microposts.each do |micropost|
-        Micropost.find_by_id(micropost.id).should be_nil
-      end
-    end
+#     it "should have the right microposts in the right order" do
+#       @user.microposts.should == [newer_micropost, older_micropost]
+#     end
 
-    describe "status" do
-      let(:unfollowed_post) do
-        FactoryGirl.create(:micropost, user: FactoryGirl.create(:user))
-      end
-      let(:followed_user) { FactoryGirl.create(:user) }
+#     it "should destroy associated microposts" do
+#       microposts = @user.microposts.dup
+#       @user.destroy
+#       microposts.should_not be_empty
+#       microposts.each do |micropost|
+#         Micropost.find_by_id(micropost.id).should be_nil
+#       end
+#     end
 
-      before do
-        @user.follow!(followed_user)
-        3.times { followed_user.microposts.create!(content: "Lorem ipsum") }
-      end
+#     describe "status" do
+#       let(:unfollowed_post) do
+#         FactoryGirl.create(:micropost, user: FactoryGirl.create(:user))
+#       end
+#       let(:followed_user) { FactoryGirl.create(:user) }
 
-      its(:feed) { should include(newer_micropost) }
-      its(:feed) { should include(older_micropost) }
-      its(:feed) { should_not include(unfollowed_post) }
-      its(:feed) do
-        followed_user.microposts.each do |micropost|
-          should include(micropost)
-        end
-      end
-    end
-  end
+#       before do
+#         @user.follow!(followed_user)
+#         3.times { followed_user.microposts.create!(content: "Lorem ipsum") }
+#       end
 
-  # =============================
-  # following validation
-  # =============================
-  describe "following" do
-    let(:other_user) { FactoryGirl.create(:user) }    
-    before do
-      @user.save
-      @user.follow!(other_user)
-    end
+#       its(:feed) { should include(newer_micropost) }
+#       its(:feed) { should include(older_micropost) }
+#       its(:feed) { should_not include(unfollowed_post) }
+#       its(:feed) do
+#         followed_user.microposts.each do |micropost|
+#           should include(micropost)
+#         end
+#       end
+#     end
+#   end
 
-    it { should be_following(other_user) }
-    its(:followed_users) { should include(other_user) }
+#   # =============================
+#   # following validation
+#   # =============================
+#   describe "following" do
+#     let(:other_user) { FactoryGirl.create(:user) }
+#     before do
+#       @user.save
+#       @user.follow!(other_user)
+#     end
 
-    describe "followed user" do
-      subject { other_user }
-      its(:followers) { should include(@user) }
-    end
+#     it { should be_following(other_user) }
+#     its(:followed_users) { should include(other_user) }
 
-    describe "and unfollowing" do
-      before { @user.unfollow!(other_user) }
+#     describe "followed user" do
+#       subject { other_user }
+#       its(:followers) { should include(@user) }
+#     end
 
-      it { should_not be_following(other_user) }
-      its(:followed_users) { should_not include(other_user) }
-    end
-  end
-end
+#     describe "and unfollowing" do
+#       before { @user.unfollow!(other_user) }
+
+#       it { should_not be_following(other_user) }
+#       its(:followed_users) { should_not include(other_user) }
+#     end
+#   end
+# end
